64c044df1ec4406d9f32df63d8aeb608a156220c
User Name <Email@example.com>
2023-07-06 07:20:00 +0200

    "signature"

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SignaturePadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('laravel-signature-pad', [SignaturePadController::class, 'index']);
+Route::get('laravel-signature-pad-company', [SignaturePadController::class, 'index_company']);
+Route::post('laravel-signature-pad', [SignaturePadController::class, 'store']);
+
+//require __DIR__.'/auth.php';
