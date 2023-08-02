3cee5a4b2267d47dbcce6eb161ceea1a97f48067
User Name <Email@example.com>
2023-08-02 22:36:30 +0200

    "update paymentSchedule"

--- app/Http/Resources/Quote/AttachmentResource.php
+++ app/Http/Resources/Quote/AttachmentResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\quote;
 
-use App\Http\Resources\team\AttachmentVideosResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AttachmentResource extends JsonResource
